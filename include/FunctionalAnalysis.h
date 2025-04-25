53262a361bc6ed41771458d65fb6b0a63baa6f5b
riops <berkozcn@gmail.com>
2025-04-25 17:40:07 +0300

    Solved the header file error.

--- include/FunctionalAnalysis.h
+++ include/FunctionalAnalysis.h
@@ -22,15 +22,21 @@ long double AdjustStepSize(long double dt, long double error,
 std::vector<std::vector<long double>> RungeKutta45(
     std::function<std::vector<long double>(const std::vector<long double> &)> f,
     const std::vector<long double> &point, long double dt);
+std::vector<std::vector<long double>>
+RungeKutta45Mpi(const std::function<std::vector<long double>(
+                    const std::vector<long double> &)> &f,
+                const std::vector<long double> &point_full, long double dt);
 
 std::vector<std::vector<long double>> IntegrateSystem45(
     std::function<std::vector<long double>(const std::vector<long double> &)> f,
     const std::vector<long double> &point, long double dt, int numSteps,
     bool allValues);
-std::vector<std::vector<long double>> IntegrateSystem45Mpi(
-    std::function<std::vector<long double>(const std::vector<long double> &)> f,
-    const std::vector<long double> &point, long double dt, int numSteps,
-    bool allValues);
+
+std::vector<std::vector<long double>>
+IntegrateSystem45Mpi(const std::function<std::vector<long double>(
+                         const std::vector<long double> &)> &f,
+                     const std::vector<long double> &init, long double dt,
+                     int numSteps, bool allValues);
 
 std::vector<std::vector<long double>> IntegrateSystem4(
     std::function<std::vector<long double>(const std::vector<long double> &)> f,
