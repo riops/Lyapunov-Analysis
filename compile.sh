a4bad12126cac2dd83720241174589f9daf3c24a
riops <berkozcn@gmail.com>
2025-05-12 17:16:36 +0300

    Changed the plot_ui.py script

--- compile.sh
+++ compile.sh
@@ -4,5 +4,5 @@ module load comp/gcc/12.3.0
 module load lib/openmpi/4.1.6
 module load lib/gsl/2.7.0
 
-# mpicxx -O3 -march=native -fopenmp -std=c++17 -I./include src/*.cpp -o builds/main
-mpicxx -O3 -march=native -fopenmp -std=c++17 -I./include src/*.cpp -lgsl -lgslcblas -o builds/main
+# mpicxx -O3 -march=native -fopenmp -std=c++17 -I./include src/*.cpp -lgsl -lgslcblas -o builds/main
+mpicxx -O3 -march=native -fopenmp -std=c++17 $(gsl-config --cflags) src/*.cpp $(gsl-config --libs) -o builds/main
